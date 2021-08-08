0bc6353d53a173591f278c55abb61b815ed0d44e
Zack Tillotson <ZackTillotson@gmail.com>
2021-08-08 15:25:37 -0600

    Add images to the cache (in sw)

--- public/sw-images.js
+++ public/sw-images.js
@@ -21,7 +21,11 @@ self.addEventListener('fetch', event => {
         }
 
         console.log(cacheVersion, 'fetch', 'didn\'t find response in cache, doing fetch');
-        return fetch(event.request);
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone())
+            return response
+          })
       })
   )
 });
